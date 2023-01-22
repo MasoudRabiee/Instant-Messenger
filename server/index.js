efdec258ccfa2f1f5d1cfe3f5bfa0a0827495f7e
MasoudRabiee <masoud.rabiee20@gmail.com>
2023-01-23 01:14:13 +0330

    add mongoose and database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// DataBase :
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
