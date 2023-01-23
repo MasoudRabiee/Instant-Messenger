9084cbb640479da8c72034c80b4cd71c9cab081f
MasoudRabiee <masoud.rabiee20@gmail.com>
2023-01-23 15:06:29 +0330

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 export default function App() {
   return (
     <BrowserRouter>
-
-
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
